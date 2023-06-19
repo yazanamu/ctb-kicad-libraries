44f61b757daf61af07bac5690a1349be15cdf260
wsjang <wsjang@everytalk.co.kr>
2023-06-19 16:16:42 +0900

    Updated.

---
+++ fetch.cmd
@@ -0,0 +1,12 @@
+cd..
+cd kicad-symbols
+git fetch
+cd ../kicad-footprints
+git fetch
+cd ../kicad-packages3D
+git fetch
+cd ../kicad-templates
+git fetch
+cd ../ctb-kicad-libraries
+git fetch
+@pause
